d28041fc3bc2c2fe5218e0c50949f6dcc263f6f5
Dmitry Motylev <dmitry@redsift.io>
2021-02-18 12:02:03 +0000

    replace single rand with pool

--- references.go
+++ references.go
@@ -1,8 +1,10 @@
 package foodfans
 
 import (
+	"encoding/binary"
 	"math/rand"
 	"strings"
+	"sync"
 	"time"
 
 	. "github.com/redsift/go-foodfans/lookup"
@@ -10,26 +12,43 @@ import (
 
 const sep = "_"
 
-var _default=NewFoodFans(rand.NewSource(time.Now().UnixNano()))
+var _default = NewFoodFans(rand.NewSource(Seed()))
 
 func New() string {
 	return _default.New()
 }
 
 type FoodFans struct {
-	rand *rand.Rand
+	rnd *sync.Pool
 }
 
 func NewFoodFans(src rand.Source) *FoodFans {
-	return &FoodFans{rand: rand.New(src)}
+	return &FoodFans{
+		rnd: &sync.Pool{New: func() interface{} { return rand.New(src) }},
+	}
 }
 
-func (f* FoodFans) New() string {
+func (f *FoodFans) New() string {
+	rng := f.rnd.Get().(*rand.Rand)
+
 	var w strings.Builder
-	w.WriteString(Verb(f.rand.Intn(int(LastVerb))).String())
+
+	w.WriteString(Verb(rng.Intn(int(LastVerb))).String())
 	w.WriteString(sep)
-	w.WriteString(Adjective(f.rand.Intn(int(LastVerb))).String())
+	w.WriteString(Adjective(rng.Intn(int(LastVerb))).String())
 	w.WriteString(sep)
-	w.WriteString(Noun(f.rand.Intn(int(LastVerb))).String())
+	w.WriteString(Noun(rng.Intn(int(LastVerb))).String())
+
+	f.rnd.Put(rng)
+
 	return w.String()
+}
+
+func Seed() int64 {
+	var seed [8]byte
+	if _, err := rand.Read(seed[:]); err != nil {
+		panic("cannot seed math/rand package with cryptographically secure random number generator")
+	}
+
+	return time.Now().UnixNano() * int64(binary.LittleEndian.Uint64(seed[:]))
 }
