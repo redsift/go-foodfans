7af969f039c6f60aa02bcb4c132b62fdf9e8205a
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2021-02-18 17:11:09 +0000

    added a unit test

--- references_test.go
+++ references_test.go
@@ -1,26 +1,32 @@
 package foodfans_test
 
 import (
-	"fmt"
-	"github.com/redsift/go-foodfans"
 	"math/rand"
+	"testing"
+
+	"github.com/redsift/go-foodfans"
 )
 
-func ExampleFoodFans_New() {
+func TestExampleFoodFans_New(t *testing.T) {
+	want := []string {
+		"cut_bland_cabbage",
+		"separate_flavorful_cumquat",
+		"separate_boiled_anchovies",
+		"salt_chunked_coconut",
+		"discard_clove_coated_cod",
+		"divide_acrid_aero_chocolate_bar",
+		"puree_chunked_cauliflower",
+		"mound_gingery_buffalo_wings",
+		"tilt_candied_basil",
+		"soak_caramelized_cumquat",
+	}
+
 	foodfans.New()
 	ff := foodfans.NewFoodFans(rand.NewSource(0))
 	for i:=0; i<10; i++ {
-		fmt.Println(ff.New())
+		got := ff.New()
+		if got != want[i] {
+			t.Errorf("want %s, got %s", want[i], got)
+		}
 	}
-	// Output:
-	// cut_bland_cabbage
-	// separate_flavorful_cumquat
-	// separate_boiled_anchovies
-	// salt_chunked_coconut
-	// discard_clove_coated_cod
-	// divide_acrid_aero_chocolate_bar
-	// puree_chunked_cauliflower
-	// mound_gingery_buffalo_wings
-	// tilt_candied_basil
-	// soak_caramelized_cumquat
 }
